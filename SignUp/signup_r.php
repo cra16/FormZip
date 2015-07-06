0f6aa466d772b6da70a7dab130bc00da4d6dfe97
world <yyll9933@gmail.com>
2015-07-06 09:24:29 +0900

    데이터베이스로 정보를 전송해주는 파일

---
+++ SignUp/signup_r.php
@@ -0,0 +1,24 @@
+<?php
+$connect=mysqli_connect('52.69.55.228', 'root', 'helloworld206');
+
+mysqli_select_db($connect,'test');
+switch($_GET['mode']){
+    case 'insert':
+        $info = "INSERT INTO member (id, password, name, s_number, p_number)
+         VALUES ('".mysqli_real_escape_string($connect,$_POST['id'])."', '".mysqli_real_escape_string($connect,$_POST['pass'])."'
+                   ,'".mysqli_real_escape_string($connect,$_POST['name'])."','".mysqli_real_escape_string($connect,$_POST['stunum'])."'
+                   ,'".mysqli_real_escape_string($connect,$_POST['phone'])."' )" ; 
+         
+        $result = mysqli_query($connect,$info);
+         //header("Location: list.php");
+        break;
+   /* case 'delete':
+        mysql_query('DELETE FROM topic WHERE id = '.mysql_real_escape_string($_POST['id']));
+        header("Location: list.php"); 
+        break;
+    case 'modify':
+        mysql_query('UPDATE topic SET title = "'.mysql_real_escape_string($_POST['title']).'", description = "'.mysql_real_escape_string($_POST['description']).'" WHERE id = '.mysql_real_escape_string($_POST['id']));
+        header("Location: list.php?id={$_POST['id']}");
+        break;*/
+}  mysqli_close($connect);
+?>
