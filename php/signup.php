9ded69fb5a63f7a4a73e48131fefe8c50e73259a
world <yyll9933@gmail.com>
2015-07-08 17:22:25 +0900

    signup page 끝

--- php/signup.php
+++ php/signup.php
@@ -6,11 +6,11 @@ mysqli_query("set session character_set_connection=utf8;");
 mysqli_query("set session character_set_results=utf8;");
 mysqli_query("set session character_set_client=utf8;");
 
-$connect=mysqli_connect('DB_HOST', 'DB_USER', 'DB_PASSWORD');
+$connect=mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD);
 mysqli_set_charset($connect, "utf8");
 
 
-mysqli_select_db($connect,'DB_NAME');
+mysqli_select_db($connect,DB_NAME);
 switch($_GET['mode']){
     case 'insert':
         $info = "INSERT INTO student (id, password, name)
