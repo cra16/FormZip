d26101d5c3aaadccfae17c8535f4edcd09b3d26c
world <yyll9933@gmail.com>
2015-07-09 19:11:12 +0900

    데이터 베이스를 웹브라우져상에 표 형식으로 나타내면 그걸 엑셀파일로 다운받을 수 있게 해주는 파일 나중에 지원서 목록 작성할 때 링크 걸면 됩니다.

---
+++ php/download.php
@@ -0,0 +1,53 @@
+<?php
+  require_once('DB_INFO.php');
+  Header("Content-type: application/vnd.ms-excel");
+  Header("Content-type: charset=utf-8");
+  header("Content-Disposition: attachment; filename=Download.xls");
+ Header("Content-Description: PHP5 Generated Data");
+  Header("Pragma: no-cache");
+  Header("Expires: 0");
+
+ 
+
+  $connect=mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD);
+  mysqli_select_db($connect,DB_NAME);
+  $result = mysqli_query($connect,'SELECT * FROM student'); 
+   
+
+?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+  </head>
+  <body>
+<table border=1 cellpadding=2 cellspacing=5>
+
+<tr bgcolor=#DDDDDD>
+<td>index</td>
+<td>name</td>
+<td>id</td>
+<td>password</td>
+</tr>
+<?php
+while($array = mysqli_fetch_array($result)){
+?>
+    <tr>
+        <td width=90>
+            <p align=center><?php echo $array[index];?></p>
+        </td>
+        <td width=250>
+            <p align=center><?php echo $array[student_name];?></p>
+        </td>
+        <td width=50>
+            <p align=center><?php echo $array[id];?></p>
+        </td>
+        <td width=70>
+            <p align=center><?php echo $array[password];?></p>
+        </td>       
+    </tr>      
+<?php
+   }
+?>
+  </body>
+</html>
