55a208f3e158807c0a2d174e45f90b36455d4bbc
haein722 <haein722@hanmail.net>
2015-07-10 11:23:26 +0900

    search

---
+++ php/clublist.php
@@ -0,0 +1,16 @@
+<div>
+    <form class="search" action='dong.php' method='GET'>
+      <fieldset>
+       <select class="search-category">
+        <option value="perform-cate">공연/음악</option>
+        <option value="sport-cate">스포츠</option>
+        <option value="academic-cate">학술</option>
+        <option value="etc-cate">기타</option>
+       </select>
+       <input type="text" name='search' class="search-text">
+       
+       <input type="submit" name='submit' class="search-button" value="검색">
+
+      </fieldset>
+    </form>
+   </div>
