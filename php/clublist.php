b97c2979ad02ec293e028966eeb25a847dbc6c44
LeeDongYoung <dyl159@naver.com>
2015-07-17 16:05:19 +0900

    club 다시!@!

--- php/clublist.php
+++ php/clublist.php
@@ -1,8 +1,9 @@
 <?php
+session_start();
   $mysql_hostname = "localhost";      
   $mysql_user = "root";
-  $mysql_password = "78910";    
-  $mysql_database = "meeting";
+  $mysql_password = "gksehdeo357";    
+  $mysql_database = "formzip";
   $prefix = "";
   $bd = mysql_connect($mysql_hostname, $mysql_user, $mysql_password) or die("Could not connect database");
         mysql_select_db($mysql_database, $bd) or die("Could not select database"); 
@@ -15,12 +16,12 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-    <title>Academy List</title>
+    <title>Club List</title>
 
     <!-- Bootstrap -->
-    <link href="bootstrap.min.css" rel="stylesheet">
-    <link href="bootstrap.css" rel="stylesheet">
-    <link href="clublist.css" rel="stylesheet">
+    <link href="../css/bootstrap.min.css" rel="stylesheet">
+    <link href="../css/bootstrap.css" rel="stylesheet">
+    <link href="../css/clublist.css" rel="stylesheet">
 
   </head>
 <body>
@@ -42,7 +43,15 @@
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
      
       <ul class="nav navbar-nav navbar-right">
-        <li><a href="login.php">Login</a></li>
+        <li>
+          <?php
+            if($_SESSION['USER_NAME'])
+              echo '<a href="logout.php">Logout</a>';
+            else
+              echo '<a href="login.php">Login</a>';
+          ?>
+
+        </li>       
         <li><a href="signup.php">Signup</a></li>
         <li><a href="#">Help</a></li>
       </ul>
@@ -51,35 +60,29 @@
   </nav>
    <!-- Menubar end-->  
 
-   <h2>학회 검색</h2>
+   <h2>동아리 검색</h2>
 
    <!-- Club Search Bar-->
    <div>
-    <form action='aa.php' method='GET'>
+    <form action='clublist.php' method='GET'>
       <fieldset>
         <input type = 'submit' class = "search-button" name ='whole' value ='전체' >
-        <input type = 'submit' class = "search-button" name ='GLS' value ='글로벌리더쉽' >
-        <input type = 'submit' class = "search-button" name ='international' value ='국제어문' >
-        <input type = 'submit' class = "search-button" name = 'management' value ='경영경제' >
-        <input type = 'submit' class = "search-button" name = 'law' value ='법' >
-        <input type = 'submit' class = "search-button" name = 'communication_study' value ='언론정보'>
-        <input type = 'submit' class = "search-button" name = 'partial_environment' value ='상담복지' >
-        <input type = 'submit' class = "search-button" name = 'life_sci' value ='생명과학' >
-        <input type = 'submit' class = "search-button" name = 'im_design' value ='공간시스템' >
-        <input type = 'submit' class = "search-button" name = 'computer_science' value ='전산전자' >
-        <input type = 'submit' class = "search-button" name ='industrial_edu' value ='산업디자인' >
-        <input type = 'submit' class = "search-button" name ='GEA' value ='글로벌에디슨아카데미' >
-        <input type = 'submit' class = "search-button" name ='ICT' value ='창의융합교육원' >
+        <input type = 'submit' class = "search-button" name = 'perform' value ='공연/음악' >
+        <input type = 'submit' class = "search-button" name = 'sport' value ='스포츠'>
+        <input type = 'submit' class = "search-button" name = 'academic' value ='학술' >
+        <input type = 'submit' class = "search-button" name = 'computer' value ='전산' >
+        <input type = 'submit' class = "search-button" name = 'religion' value ='종교' >
+        <input type = 'submit' class = "search-button" name = 'volunteer' value ='봉사' >
     </form>
      </fieldset>
    </div>
 
 <?php
-$name_arr=array("whole","GLS","international","management","law","communication_study","partial_environment","life_sci","im_design","computer_science","industrial_edu","GEA","ICT");
+$name_arr=array("whole","perform","sport","academic","computer","religion","volunteer");
 
-for($i=0; $i<13;$i++){
+for($i=0; $i<7;$i++){
   if($_GET[$name_arr[$i]]){
-  $condition = $_GET[$name_arr[$i]];
+    $condition = $_GET[$name_arr[$i]];
     break;
   }
 }
@@ -89,33 +92,37 @@ if($condition == NULL){
 }
 
 if( $condition != "전체" ){
-  $sql="SELECT academy_name FROM academy WHERE dept='$condition'";  
+  $sql="SELECT c_name FROM club WHERE field='$condition'";  
 }else{
-  $sql = "SELECT academy_name FROM academy";
+  $sql = "SELECT c_name FROM club";
 } 
 $result=mysql_query($sql);
 
 $i=0;
 $j=0;
-$academy[$i] = "dd";
+$clubname[$i] = "dd";
 ?>
 
-<form action = "dd.php">
-<?php
-while($academy[$i] != NULL){
+
+<form action="clubpage.php" method="POST">
+  <?php
+while($clubname[$i] != NULL){
   echo "<tr>";
   for($j=0 ; $j<4 ; $j++){
     echo "<td>";
-    $academy = mysql_fetch_array($result);
-    if($academy[$i] == NULL){
+    $clubname = mysql_fetch_array($result);
+    if($clubname[$i] == NULL){
       break;
     }
-    echo "<input class = 'club-element' type = 'submit' value ='$academy[$i]' name='name'/>";
+    echo "<input class = 'club-element' type = 'submit' value ='$clubname[$i]' name = 'name'/>";
     echo "</td>";
     }
     echo "<br/>";
     echo "</tr>";
     }
+
+    //$condition = NULL;
+  
 
 ?>
 </form>
