a703d77d54ce9924edd8f0ce9fa8a85ee48eb2d2
LeeDongYoung <dyl159@naver.com>
2015-07-22 17:38:12 +0900

    app_list

--- php/app_list.php
+++ php/app_list.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
-  // Manager judge
-  require_once('auth.php');
+// Manager judge
+require_once('auth.php');
 require_once('DB_INFO.php');
 header('Content-Type: text/html; charset=utf-8');
 
@@ -76,49 +76,129 @@ mysqli_select_db($bd,DB_NAME) or die("Could not select database");
     </tr>
   </thead>
   <tbody>
-    <?php
-    //CLUB get하기
-      $club_name= $_GET['name'];
-     
-      $qry1 = "SELECT name FROM result WHERE club_name = '$club_name' ";
-      $qry2 = "SELECT stu_id FROM result WHERE club_name = '$club_name' ";
-      $qry3 = "SELECT gender FROM result WHERE club_name = '$club_name' ";
-      $name_result = mysqli_query($bd,$qry1);
-      $id_result = mysqli_query($bd,$qry2);
-      $gender_result = mysqli_query($bd,$qry3);
 
-      $i=0;
-      $j=1;
-      $name = 'dd';
-      while($name[$i]!=NULL){
-        $name = mysqli_fetch_array($name_result);
-        if($name[$i] == NULL){
-          break;
-        }
-        $id = mysqli_fetch_array($id_result);
-        $gender = mysqli_fetch_array($gender_result);      
+<?php
+$club_name=$_SESSION['USER_NAME'];
+$qry="SELECT * FROM result WHERE club_name='$club_name'";   
+$result=mysqli_query($bd,$qry);
+
+$per_page=5;  //page당 display할 목록의 수
+$total_results=mysqli_num_rows($result);  //해당 동아리의 지원자 수
+$total_pages=ceil($total_results/$per_page); 
+$result=$_GET['currentpage'];
+
+// get the current page or set a default
+if (isset($_GET['currentpage']) && is_numeric($_GET['currentpage'])) {
+   // cast var as int
+   $currentpage = (int) $_GET['currentpage'];
+} else {
+   // default page num
+   $currentpage = 1;
+} // end if
+
+// if current page is greater than total pages...
+if ($currentpage > $total_pages) {
+   // set current page to last page
+   $currentpage = $total_pages;
+} // end if
+// if current page is less than first page...
+if ($currentpage < 1) {
+   // set current page to first page
+   $currentpage = 1;
+} // end if
+
+// the offset of the list, based on current page 
+$offset = ($currentpage - 1) * $per_page;
+
+// get the info from the db 
+$sql = "SELECT * FROM result LIMIT $offset, $per_page";
+$result=mysqli_query($bd,$sql);
+
+while ($list = mysqli_fetch_assoc($result)) {
+  $stu_id = $list['stu_id'];
+  $name = $list['name'];
+  $gender=$list['gender']; 
+
 ?>
-
        <tr id = '$j' class = 'clickable-row' data-href='firstpage.html'>
        <td class = 'studnet-number' scope='row'>
-        <?php echo"$id[$i]"; ?> 
+        <?php echo "$stu_id"; ?> 
        </td>
        <td class = 'Name'>
-        <?php echo"$name[$i]"; ?>
+        <?php echo "$name"; ?>
        </td>
        <td class = 'sex'>
-        <?php echo"$gender[$i]"; ?>
+        <?php echo "$gender"; ?>
        </td>
        </tr>
 
       <?php
-       // $i++;
-        $j++;
       }
-
-    ?> 
+         
+      ?> 
   </tbody>
 </table>
+
+
+
+<ul class="pagination">
+<?php
+// get the current page or set a default
+if (isset($_GET['currentpage']) && is_numeric($_GET['currentpage'])) {
+  $currentpage = (int) $_GET['currentpage'];
+
+  if($currentpage > 0 && $currentpage <= $total_pages)
+  {
+    $start=floor(($currentpage-1)/4)*4+1;
+    $end= $start+4;
+  }
+
+  else
+  {
+    $start=1;
+    $end=$start+4;
+  }
+}
+
+else{
+  $start=1;
+  $end=$start+4;
+}
+
+if($start>1){
+?>
+  <li><a href="app_list.php?currentpage=<?php echo $start-4;?>">«</a></li>
+<?php
+}
+
+?>
+
+
+  
+
+<?php
+
+  for($i=$start; $i<=$end;$i++)
+  {
+    if($i > $total_pages)
+    {
+      break;
+    }
+?>
+    <li><a href="app_list.php?currentpage=<?php echo $i;?>"><?php echo $i;?></a></li>
+<?php
+  }
+
+  if($end<$total_pages){
+?>
+  <li><a href="app_list.php?currentpage=<?php echo $start+$per_page;?>">»</a></li>
+<?php
+}
+
+?>
+
+</ul>
+
 </div>
 
     <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
