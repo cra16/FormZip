34a80189463fddb245afbcc37b1a5e6b065fae3a
Eunbee Jo <eunbeejo94@gmail.com>
2015-07-23 16:50:16 +0900

    마이페이지 조금 수정

--- php/mypage.php
+++ php/mypage.php
@@ -2,8 +2,6 @@
   // Session start 
   session_start();
 
-
-/*
   // DB connection
   require_once('DB_INFO.php');
   header('Content-Type: text/html; charset=utf-8');
@@ -17,41 +15,9 @@
 
   mysqli_select_db($bd,DB_NAME) or die("Could not select database");
 
-  $user_id= $_SESSION["USER_NAME"]; 
-
-  $qry="SELECT * FROM application WHERE id='$user_id'";   
-  $result=mysqli_query($bd,$qry);
-
-  //Check whether the query was successful or not
-  if($result) {
-
-      if(mysqli_num_rows($result) > 0) 
-      {
-        $member = mysqli_fetch_assoc($result);
-        
-      }
-
-      else 
-      {
-       echo "Data call failed";
-      }
-  }
-  else 
-  {
-    die("Query failed");
-  }
+ ?>
 
 
-//Sanitize the POST values
-  $short_info=array("use","use","use","use","use",$member['served'],$member['mail'],$member['activity']);
-  $sub_info=array($member['sr1'],$member['sr2'],$member['sr3'],$member['sr4'],$member['sr5'],$member['sr6'],$member['sr7']);
-  $label_name = array("이름","학번","학과","전화번호","성별","군필여부","e-mail","활동가능학기");
-  $question_placeholder= array("Name","Student ID","Major ex) 1전공/2전공","Phone number ex)01012345678","남/여","남성인 경우만 해당","ex)formzip@naver.com","ex)3학기");
-  $title=array($member['title1'],$member['title2'],$member['title3'],$member['title4'],$member['title5'],$member['title6'],$member['title7']);
-  $explain=array($member['explain1'],$member['explain2'],$member['explain3'],$member['explain4'],$member['explain5'],$member['explain6'],$member['explain7']);
-
-*/
-?>
 <!DOCTYPE HTML> 
 <html>
 
@@ -95,21 +61,39 @@
    <table class = "personal">
     <tr>
       <td class="header"><label>이름</label></td>
-      <td class = "content"> <input type="text"  value = "<?php      ?>" class="fixed-name"  placeholder="<?php echo $question_placeholder[$i]; ?>"> </td>
+      <td class = "content"> 
+
+<?php
+  $id="haein722"; // session 
+  $qry="SELECT * FROM student WHERE id='$id'";   
+  $result=mysqli_query($bd,$qry);
+  $list = mysqli_fetch_array($result);
+  $name = $list['student_name'];
+  $bday = $list['birth'];
+  echo "<input type='text' value = '$name' disabled >";
+?>
+      </td>
     </tr>
     <tr>
       <td class="header"><label>ID</label></td>
-      <td class = "content"> <input type="text" value = "<?php      ?>" class="fixed-id"  placeholder="<?php echo $question_placeholder[$i]; ?>"> </td>
+      <td class = "content"> 
+<?php
+  echo "<input type='text' value = '$id' disabled >";
+?>
+      </td>
     </tr>
     <tr>
       <td class="header"><label>생년월일</label></td>
       <td class = "bday"> 
-        <input type="text" value = "<?php      ?>" class="fixed-bday"  placeholder="<?php echo $question_placeholder[$i]; ?>">
-
-        <input type="text" value = "<?php      ?>" class="fixed-bday"  placeholder="<?php echo $question_placeholder[$i]; ?>">
-
-        <input type="text" value = "<?php      ?>" class="fixed-bday"  placeholder="<?php echo $question_placeholder[$i]; ?>">
-
+<?php
+  $year = 1900 + $bday/10000;
+  $month = ($bday/100)%100;
+  $day = $bday%10000;
+  echo "<input type='text' value = '1994' class='fixed-bday'>";
+  echo "<input type='text' value = '07' class='fixed-bday'>";
+  echo "<input type='text' value = '22' class='fixed-bday'>";
+  
+?>
       </td>
     </tr>
     <tr>
@@ -156,26 +140,33 @@
         </tr>
       </thead>
       <tbody>
-        <tr id = "01" class = "clickable-row" data-href="<?php   //지원서 링크 넣기    ?>">
-          <th class = "app-number" scope="row">1</th>
-          <td class = "app-club">어메이징 스토리</td>
-          <td class = "app-status">작성중</td>
+
+<?php
+
+  $i = 0;
+  $j = 1;
+  $id = "21300739";
+  $qry2 = "SELECT * FROM result WHERE stu_id = '$id'";
+  $result=mysqli_query($bd,$qry2);
+
+  while($list = mysqli_fetch_array($result)){
+    $clubname = $list['club_name'];
+  ?>
+        <tr id = '$j' class = "clickable-row" data-href='firstpage.html'>
+        <th class = "app-number" scope="row">
+          <?php echo "$j";?>
+        </th>
+         <td class = "app-club">
+          <?php echo "$clubname"; ?>
+         </td>
+        <td class = "app-status">
+          <?php echo "제출중"; ?>
+        </td>
         </tr>
-        <tr id = "02" class = "clickable-row" data-href="<?php   //지원서 링크 넣기    ?>" >
-          <th class = "app-number" scope="row">2</th>
-          <td class = "app-club">꾼들 </td>
-          <td class = "app-status">제출 완료</td>
-        </tr>
-        <tr id = "03" class = "clickable-row" data-href="<?php   //지원서 링크 넣기   ?>">
-          <th class = "app-number" scope="row">3</th>
-          <td class = "app-club">즉흥적 새벽 두시</td>
-          <td class = "app-status">작성중</td>
-        </tr>
-        <tr id = "06" class = "clickable-row" data-href="<?php   //지원서 링크 넣기   ?>">
-          <th class = "app-number" scope="row">4</th>
-          <td class = "app-club">CRA</td>
-          <td class = "app-status">제출 완료</td>
-        </tr>  
+<?php
+}
+
+?> 
       </tbody>
     </table>
 </div>
