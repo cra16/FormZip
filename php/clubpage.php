4bc0a39c87a093bafa116102c4d27460ca7bde8a
world <yyll9933@gmail.com>
2015-07-25 22:28:49 +0900

    cluppage url을 바로 입력하고 가는 행위 방지

--- php/clubpage.php
+++ php/clubpage.php
@@ -23,8 +23,14 @@
   }
 
   else{
+    if($_SESSION['GROUP']){
     $club_name=$_SESSION['GROUP'];
     $_GET['name']=$club_name;
+     }
+    else{
+      header("location: firstpage.php");
+              exit();
+    }
   }
   
   $qry="SELECT * FROM club WHERE c_name='$club_name'";   
