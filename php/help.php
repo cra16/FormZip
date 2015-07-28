3d7c02c604d72e702ad8b0bbf8316c0fec428256
haein722 <haein722@hanmail.net>
2015-07-28 10:42:28 +0900

    help

---
+++ php/help.php
@@ -0,0 +1,19 @@
+<?php
+header('Content-Type: text/html; charset=utf-8');
+
+mysqli_query("set session character_set_connection=utf8;");
+mysqli_query("set session character_set_results=utf8;");
+mysqli_query("set session character_set_client=utf8;");
+?>
+<html>
+
+<head> 
+	<title> 도움말 </title>
+</head>
+
+<body>
+	1111111111111111111111;
+</body>
+
+
+</html>
