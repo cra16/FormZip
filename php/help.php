c800f75f3704db81ff94eee84399c19cdfa1b0ba
Eunbee Jo <eunbeejo94@gmail.com>
2015-07-29 17:05:51 +0900

    help 창

--- php/help.php
+++ php/help.php
@@ -5,15 +5,43 @@ mysqli_query("set session character_set_connection=utf8;");
 mysqli_query("set session character_set_results=utf8;");
 mysqli_query("set session character_set_client=utf8;");
 ?>
-<html>
-
-<head> 
-	<title> 도움말 </title>
-</head>
-
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title> Help </title>
+    <!-- Bootstrap -->
+    <link href="../css/bootstrap.min.css" rel="stylesheet">
+    <link href="../css/bootstrap.css" rel="stylesheet">
+    <link href="../css/help.css" rel="stylesheet">
+  </head>
 <body>
-	1111111111111111111111;
+ <section class = "content-text">
+  <h4 class = "title">Contact Us:</h4>
+   <ul>
+ 	<li>email: hguformzip@gmail.com
+ 	<li>cell-phone:
+ 	<br> 010-5103-2361/
+ 	<br> 010-4952-0181/
+ 	<br> 010-4857-2886 	
+   </ul>
+
+  <br><h5 class = "explanation">동아리/학회 페이지 등록 방법:</h5>
+   <ol>
+ 	<li> 아래 사항들을 정확히 기재하여 hguformzip@gmail.com 으로 메일을 보낸다.
+ 		<br>1) 동아리/학회 이름
+ 		<br>2) 희망하는 아이디
+ 		<br>3) 희망하는 비밀번호
+ 	</li>	
+ 	<li> FORM:ZIP의 Confirm을 받는다.</li>
+ 	<li> 로그인 후 해당 페이지에 접속해 페이지를 수정(소개글 및 사진)한다.
+ 	</li> 
+ 	<li> 해당 페이지에서 '지원서 양식 수정'을 통해 지원서를 만든다.
+ 	</li>
+   </ol>
+ </section>	
 </body>
-
-
 </html>
