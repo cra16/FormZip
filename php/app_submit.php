7ae84802580f88fe5f09d205f0d58b69ffccc5ba
haein722 <haein722@hanmail.net>
2015-07-29 15:30:17 +0900

    add return

--- php/app_submit.php
+++ php/app_submit.php
@@ -229,11 +229,11 @@ else
     </div>
 
     <div class="submit_content">
-      <button type="submit" name="name" onsubmit = "ok_storage()" value="<?php echo $club; ?>">임시저장</button>
+      <button type="submit" name="name" onsubmit = " return ok_storage()" value="<?php echo $club; ?>">임시저장</button>
     </div>
 
     <div class="submit_content">
-      <button type="submit" name="name" onsubmit="ok()" value="<?php echo $club; ?>">제출</button>
+      <button type="submit" name="name" onsubmit="return ok()" value="<?php echo $club; ?>">제출</button>
     </div>
   </form>
 </div>
