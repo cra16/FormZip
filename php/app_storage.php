b5303d6b88458b21046236f266701eb5f7312f79
haein722 <haein722@hanmail.net>
2015-07-30 11:00:27 +0900

    01

--- php/app_storage.php
+++ php/app_storage.php
@@ -107,7 +107,7 @@ $name = $stuid_array['student_name'];
 $sql = "SELECT * FROM result WHERE club_name = '$club' AND stu_id = '$stuid'";
 $result = mysqli_query($bd,$sql);
 
-$check = $_GET['temp'];
+$check = $_GET['real']; //제출값 return
 
 if( $check != NULL ){ //제출
   if(mysqli_num_rows($result) > 0){
