f05c367f00b34078fa92de880ccbd9476fb9fc48
haein722 <haein722@hanmail.net>
2015-07-31 14:50:29 +0900

    tab

--- php/app_submit.php
+++ php/app_submit.php
@@ -276,8 +276,7 @@ if( $info == 1){ // 정보 존재
           <div class="form-group">
             <label class="col-lg-3 control-label"><?php echo $title[$i]; ?></label>
             <div class="col-lg-8">
-            <textarea class="form-control" rows="3" name="<?php echo $text_name[$i]; ?>">
-              <?php if($text_temp[$i] != '0'){ echo $text_temp[$i]; } ?></textarea>
+            <textarea class="form-control" rows="3" name="<?php echo $text_name[$i]; ?>"><?php if($text_temp[$i] != '0'){ echo $text_temp[$i]; } ?></textarea>
             <span class="help-block"> <?php echo $explain[$i]; ?></span>    
             </div>
           </div>  
