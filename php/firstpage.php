8cdd8f07940f1e816d82dca550d17814cc39b1d1
haein722 <haein722@hanmail.net>
2015-07-31 16:15:25 +0900

    help

--- php/firstpage.php
+++ php/firstpage.php
@@ -32,7 +32,7 @@ session_start();
 
     <script type="text/javascript">
       function help(){
-        window.open("help.php","도움말", "left=200, top=200, width=400, height=470 , scrollbars=no, resizable=yes");
+        window.open("help.php","도움말", "left=200, top=200, width=520, height=620 , scrollbars=no, resizable=yes");
       }
     </script>
 
