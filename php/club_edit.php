3eaa38e0f6598e0c5348925166921fb29c6f166d
haein722 <haein722@hanmail.net>
2015-08-03 15:49:08 +0900

    jsjs

--- php/club_edit.php
+++ php/club_edit.php
@@ -103,6 +103,6 @@ alert(document.forms[0].post-title.value);    // title에 입력된 값을 출�
     <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
     <!-- Include all compiled plugins (below), or include individual files as needed -->
-    <script src="js/bootstrap.min.js"></script>
+    <script src="../js/bootstrap.min.js"></script>
   </body>
 </html>
