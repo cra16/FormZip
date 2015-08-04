1d18b8e236bf3b225ee163253f33bec5d89e95c7
Eunbee Jo <eunbeejo94@gmail.com>
2015-08-04 15:01:05 +0900

    firstpage

--- php/firstpage.php
+++ php/firstpage.php
@@ -1,8 +1,20 @@
 <?php
 //Start the session
 session_start();
-?>
 
+// DB connection
+  require_once('DB_INFO.php');
+  header('Content-Type: text/html; charset=utf-8');
+
+  mysqli_query("set session character_set_connection=utf8;");
+  mysqli_query("set session character_set_results=utf8;");
+  mysqli_query("set session character_set_client=utf8;");
+
+  $bd=mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD) or die("Could not connect database");
+  mysqli_set_charset($bd, "utf8");
+
+  mysqli_select_db($bd,DB_NAME) or die("Could not select database");
+?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -20,12 +32,15 @@ session_start();
 
     <script type="text/javascript">
       function help(){
-        window.open("help.php","도움말", "left=200, top=200, width=350, height=420 , scrollbars=no, resizable=yes");
+        window.open("help.php","도움말", "left=200, top=200, width=520, height=620 , scrollbars=no, resizable=yes");
+      }
+      function explain(){
+        window.open("explain.php","도움말", "left=200, top=200, width=520, height=620 , scrollbars=no, resizable=yes");
       }
     </script>
 
   </head>
-  <body>
+  <body onload = "explain()">
  <!-- Menubar start-->  
   <nav class="navbar navbar-default">
   <div class="container-fluid">
@@ -53,11 +68,28 @@ session_start();
         </li>
         <li>
           <?php
-            if($_SESSION['USER_NAME'])
-              echo '<a href="mypage.php">My Page</a>';
+            //로그인 여부 확인
+           if($_SESSION['USER_NAME']){
+            $id = $_SESSION['USER_NAME'];
+            $sql = "SELECT * FROM student WHERE id = '$id'";
+            $check_result = mysqli_query($bd,$sql);
+            $check = mysqli_fetch_array($check_result);
+            $index = $check['index'];
+            $cname = $check['c_name'];
+            //관리자 여부 확인
+              if($index == 0){
+                echo '<a href="mypage.php">My Page</a>';
+               
+              }
+              else if($index ==1){
+                echo '<a href="clubpage.php?name='.$cname.'">Club Page</a>'; 
+              }
+              else{
+                echo '<a href="academypage.php?name='.$cname.'">Academy Page</a>';   
+              }
+            }
             else
-              echo '<a href="signup.php">Sign Up</a>';
-
+              echo '<a href="agreement.php">Sign Up</a>';
           ?>
         </li>
         <li><a href="#" onclick = "help()">Help</a></li>
@@ -73,16 +105,13 @@ session_start();
 
   <div class = "academy-button"></div>
   <a class="academy-button" href="../php/academylist.php"></a>   
+   
+ 
 
-   <!-- 
-  <div id="popup" style="position: absolute; top: 80px; left: 1px; width: 285px; height: 495px; z-index: 101; display: block; overflow: hidden;">
-    <img src="../img/popup.png" class = "popup">
-  </div>
- -->
 
     <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
     <!-- Include all compiled plugins (below), or include individual files as needed -->
-    <script src="js/bootstrap.min.js"></script>
+    <script src="../js/bootstrap.min.js"></script>
   </body>
 </html>
