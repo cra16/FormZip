7ee6601d8d7025b0021b4bc9fe38ec3ab154a53a
world <yyll9933@gmail.com>
2015-08-10 13:51:28 +0900

    exe한글

--- php/clubexec.php
+++ php/clubexec.php
@@ -15,6 +15,7 @@ mysqli_query("set session character_set_client=utf8;");
 
 // Create connection
 $conn = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+mysqli_set_charset($conn, "utf8");
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
