1081b1a7256a87c044f78c7f42a50e52834c1006
LeeDongYoung <dyl159@naver.com>
2015-12-30 00:00:20 +0900

    다시 올립니당

--- php/firstpage.php
+++ php/firstpage.php
@@ -41,62 +41,63 @@ session_start();
 
   </head>
   <body onload = "explain()">
- <!-- Menubar start-->  
+    
+  <!-- Menubar start-->  
   <nav class="navbar navbar-default">
-  <div class="container-fluid">
-    <div class="navbar-header">
-      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-        <span class="sr-only">Toggle navigation</span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-      </button>
-      <a class="navbar-brand" href="firstpage.php">iBELONG</a>
-    </div>
+    <div class="container-fluid">
+      <div class="navbar-header">
+        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+          <span class="sr-only">Toggle navigation</span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
+        <a class="navbar-brand" href="firstpage.php">iBELONG</a>
+      </div>
 
-    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-     
-      <ul class="nav navbar-nav navbar-right">
-        <li>
-          <?php
-            if($_SESSION['USER_NAME'])
-              echo '<a href="logout.php">Logout</a>';
-            else
-              echo '<a href="login.php">Login</a>';
-          ?>
+      <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+       
+        <ul class="nav navbar-nav navbar-right">
+          <li>
+            <?php
+              if($_SESSION['USER_NAME'])
+                echo '<a href="logout.php">Logout</a>';
+              else
+                echo '<a href="login.php">Login</a>';
+            ?>
 
-        </li>
-        <li>
-          <?php
-            //로그인 여부 확인
-           if($_SESSION['USER_NAME']){
-            $id = $_SESSION['USER_NAME'];
-            $sql = "SELECT * FROM student WHERE id = '$id'";
-            $check_result = mysqli_query($bd,$sql);
-            $check = mysqli_fetch_array($check_result);
-            $index = $check['index'];
-            $cname = $check['c_name'];
-            //관리자 여부 확인
-              if($index == 0){
-                echo '<a href="mypage.php">My Page</a>';
-               
+          </li>
+          <li>
+            <?php
+              //로그인 여부 확인
+             if($_SESSION['USER_NAME']){
+              $id = $_SESSION['USER_NAME'];
+              $sql = "SELECT * FROM student WHERE id = '$id'";
+              $check_result = mysqli_query($bd,$sql);
+              $check = mysqli_fetch_array($check_result);
+              $index = $check['index'];
+              $cname = $check['c_name'];
+              //관리자 여부 확인
+                if($index == 0){
+                  echo '<a href="mypage.php">My Page</a>';
+                 
+                }
+                else if($index ==1){
+                  echo '<a href="clubpage.php?name='.$cname.'">Club Page</a>'; 
+                }
+                else{
+                  echo '<a href="academypage.php?name='.$cname.'">Academy Page</a>';   
+                }
               }
-              else if($index ==1){
-                echo '<a href="clubpage.php?name='.$cname.'">Club Page</a>'; 
-              }
-              else{
-                echo '<a href="academypage.php?name='.$cname.'">Academy Page</a>';   
-              }
-            }
-            else
-              echo '<a href="agreement.php">Sign Up</a>';
-          ?>
-        </li>
-        <li><a href="#" onclick = "help()">Help</a></li>
-      </ul>
+              else
+                echo '<a href="agreement.php">Sign Up</a>';
+            ?>
+          </li>
+          <li><a href="#" onclick = "help()">Help</a></li>
+        </ul>
+      </div>
     </div>
-  </div>
-</nav>
+  </nav>
    <!-- Menubar end-->  
 
   <div class = "club-button"></div>
--- php/signup.php
+++ php/signup.php
@@ -11,79 +11,73 @@
     <link href="../css/bootstrap.min.css" rel="stylesheet">
     <link href="../css/bootstrap.css" rel="stylesheet">
     <link href="../css/signup.css" rel="stylesheet">
-
- 
- 
   </head>
   <body>
 
-  <!-- Logo Start -->
-  <div class="container" class = "col-lg-12 col-xs-12">
-      
-      <div id="header" class = "col-xs-8 col-xs-offset-2 col-lg-4 col-lg-offset-4 col-md-4 col-md-offset-4">
-          <a href="firstpage.php" class="h_logo">
-          <img src="../img/title.png" class = "h_logo">
+    <!-- Logo Start -->
+    <div class="col-xs-8 col-xs-offset-2 col-sm-8 col-sm-offset-2 col-md-4 col-md-offset-4 col-lg-4 col-lg-offset-4">
+      <div class ="wrapper">
+        <a href="firstpage.php">
+        <img src="../img/logo_mint.png" width="100%" >
         </a>
       </div>
-      
-  </div>
-  <!-- Logo End -->
-
+    </div>
+    <!-- Logo End -->
     
-    <div id="containbox">
-      <div class="join_content">
+
+
+    <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+      <div class="join_content col-xs-12 col-sm-6 col-sm-offset-3 col-md-4-col-md-offset-4 col-lg-4 col-lg-offset-4">
         <form action="../php/data_change.php?mode=insert" method="POST" name="myForm" onsubmit="return validateForm()">
-         <div class="form-group">
-          <h2>회원가입</h2>
-          
-          <div id="divmargin"></div>
-           <div id="divmargin"></div>
-                <label for="inputName" class="col-xs-4 col-md-3 control-label">이름</label>
-                <input class="form-control" id="name" name="name" type="text" placeholder="Name" maxlength="20" onblur="NameCheck()" >
-                <div id="divmargin"></div>
-                <div id="idMsg" class="error" style="display:none"></div>
+          <div class="form-group">
+            <h2>회원가입</h2>
+              <div id="divmargin"></div>
+             <div id="divmargin"></div>
+                  <label for="inputName" class="col-xs-4 col-md-3 control-label">이름</label>
+                  <input class="form-control" id="name" name="name" type="text" placeholder="Name" maxlength="20" onblur="NameCheck()" >
+                  <div id="divmargin"></div>
+                  <div id="idMsg" class="error" style="display:none"></div>
+                  
+                  <label for="inputName" class="col-xs-4 col-md-2 control-label">ID</label>
+                  <button class="col-md-5" id="check_button" type="button" onclick="check_id()" >중복확인</button>
+                  <input class="form-control" id="userid" name="userid" type="text" placeholder="Choose your username" maxlength="15" onblur="UserIdCheck()" >
+                  <input type="hidden" name="checkid" value=0>
+                  <div id="divmargin"></div>               
+                  <div id="userIdMsg" class="error" style="display:none"></div>
+
+                  <label for="inputName" class="col-xs-7 col-md-8 control-label">비밀번호 입력</label>
+                  <input class="form-control" id="pw" name="pw" type="password" placeholder="Create a password." maxlength="15" onblur="PWCheck()" >
+                  <div id="divmargin"></div>            
+                  <div id="pwMsg" class="error" style="display:none"></div>
                 
-                <label for="inputName" class="col-xs-4 col-md-2 control-label">ID</label>
-                <button class="col-md-5" id="check_button" type="button" onclick="check_id()" >중복확인</button>
-                <input class="form-control" id="userid" name="userid" type="text" placeholder="Choose your username" maxlength="15" onblur="UserIdCheck()" >
-                <input type="hidden" name="checkid" value=0>
-                <div id="divmargin"></div>               
-                <div id="userIdMsg" class="error" style="display:none"></div>
+                   <label for="inputName" class="col-xs-11 col-md-8 control-label">비밀번호 재입력</label>
+                  <input class="form-control" id="confirm" name="condfirm" type="password" placeholder="Confirm your password." maxlength="15" onblur="PsCfCheck()" >
+                  <div id="divmargin"></div>
 
-                <label for="inputName" class="col-xs-7 col-md-8 control-label">비밀번호 입력</label>
-                <input class="form-control" id="pw" name="pw" type="password" placeholder="Create a password." maxlength="15" onblur="PWCheck()" >
-                <div id="divmargin"></div>            
-                <div id="pwMsg" class="error" style="display:none"></div>
-              
-                 <label for="inputName" class="col-xs-11 col-md-8 control-label">비밀번호 재입력</label>
-                <input class="form-control" id="confirm" name="condfirm" type="password" placeholder="Confirm your password." maxlength="15" onblur="PsCfCheck()" >
-                <div id="divmargin"></div>
+                  <div id="pscfMsg" class="error" style="display:none"></div>
 
-                <div id="pscfMsg" class="error" style="display:none"></div>
+                  <label for="inputName" class="col-xs-7 col-md-5 control-label">학번</label>
+                  <input class="form-control" id="stuid" name="stuid" type="text" placeholder="학번 ex)21500000" maxlength="8" onblur="StuidCheck()" >
+                  <div id="divmargin"></div>
+                  
+                  <div id="stuidMsg" class="error" style="display:none"></div>
+                 
+                  <label for="inputName" class="col-xs-7 col-md-5 control-label">생년월일</label>
+                  <input class="form-control" id="birth" name="birth" type="text" placeholder="생년월일 6자리" maxlength="6" onblur="BirthCheck()" >
+                  <div id="divmargin"></div>
+                  <div id="birthMsg" class="error" style="display:none"></div>
 
-                <label for="inputName" class="col-xs-7 col-md-5 control-label">학번</label>
-                <input class="form-control" id="stuid" name="stuid" type="text" placeholder="학번 ex)21500000" maxlength="8" onblur="StuidCheck()" >
-                <div id="divmargin"></div>
-                
-                <div id="stuidMsg" class="error" style="display:none"></div>
-               
-                <label for="inputName" class="col-xs-7 col-md-5 control-label">생년월일</label>
-                <input class="form-control" id="birth" name="birth" type="text" placeholder="생년월일 6자리" maxlength="6" onblur="BirthCheck()" >
-                <div id="divmargin"></div>
-                <div id="birthMsg" class="error" style="display:none"></div>
-
-                 <div id="sbMsg" class="error2" style="display:none" ></div>
-
-            </div>
+                   <div id="sbMsg" class="error2" style="display:none" ></div>
           </div>
+      </div>
 
-          <div id="divmargin"></div>
+      <div id="divmargin"></div>
 
+        <div class="col-xs-12  col-sm-4 col-sm-offset-4 col-md-4 col-md-offset-4 col-lg-4 col-lg-off" > 
           <div class="submit_content">
-            
             <button type="submit"  onclick="IsFilled()" id="sb";>Submit</button>
           </div>
-          
+        </div>
         </form>
     </div>
 
