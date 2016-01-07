d8cc732ef8f349c4aea0a284248b04a2770e2531
world <yyll9933@gmail.com>
2016-01-07 14:44:15 +0900

    no message

--- php/DB_INFO.php
+++ php/DB_INFO.php
@@ -1,10 +1,10 @@
 <?php
   // Define database connection constants
-  define('DB_HOST', 'localhost');
-  define('DB_USER', 'root');
-  define('DB_PASSWORD', 'helloworld206');
-  define('DB_NAME', 'formzip');
-  define('KEY', '1as4fg7jk0');
+  define('DB_HOST', "localhost");
+  define('DB_USER', "root");
+  define('DB_PASSWORD', "helloworld206");
+  define('DB_NAME', "formzip");
+  define('KEY', "1as4fg7jk0");
 
   // Connect formzip
   header('Content-Type: text/html; charset=utf-8');
